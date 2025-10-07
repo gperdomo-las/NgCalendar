2463e3e9eb97ea0de9ea9fa6b572d90e4ffa9bfe
GPERDOMO <gperdomo-las111@gmail.com>
2025-10-07 12:16:38 -0400

    new begin

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,26 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { PrincipalComponent } from './components/principal/principal.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from '../app-routing.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { CabeceraComponent } from './components/cabecera/cabecera.component';
-import { PieComponent } from './components/pie/pie.component';
-import { CalendarComponent } from '../calendar/components/calendar/calendar.component';
-import { CalendarGridComponent } from '../calendar/components/calendar-grid/calendar-grid.component';
-import { CalendarDayCellComponent } from '../calendar/components/calendar-day-cell/calendar-day-cell.component';
-import { CalendarEventComponent } from '../calendar/components/calendar-event/calendar-event.component';
+
 
 
 @NgModule({
   declarations: [
-    PrincipalComponent,
-    CabeceraComponent,
-    PieComponent,
-    CalendarComponent,
-    CalendarGridComponent,
-    CalendarDayCellComponent,
-    CalendarEventComponent
+    
+
   ],
   imports: [
     CommonModule,
@@ -34,9 +23,7 @@ import { CalendarEventComponent } from '../calendar/components/calendar-event/ca
     // VisualModule
   ],
   exports: [
-    PrincipalComponent,
-    CabeceraComponent,
-    PieComponent
+    
   ],
 })
 export class CoreModule { }
