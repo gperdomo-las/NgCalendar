6be875c61e2e737d665736fede88c39f8954dcdd
GPERDOMO <gperdomo-las111@gmail.com>
2025-10-07 12:19:48 -0400

    Add header, footer, principal, and calendar components with associated templates and styles

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,6 +3,10 @@ import { CommonModule } from '@angular/common';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from '../app-routing.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { PrincipalComponent } from './components/principal/principal.component';
+import { CalendarComponent } from './components/calendar/calendar.component';
 
 
 
@@ -10,6 +14,11 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
   declarations: [
     
 
+  
+    HeaderComponent,
+               FooterComponent,
+               PrincipalComponent,
+               CalendarComponent
   ],
   imports: [
     CommonModule,
