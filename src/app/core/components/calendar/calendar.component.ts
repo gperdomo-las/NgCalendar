7c8eedd0a6ee565a7d57f184b60fc467b3544aa7
GPERDOMO <gperdomo-las111@gmail.com>
2025-10-08 10:55:12 -0400

    feat(calendar): enhance calendar grid layout and implement calendar generation logic

--- src/app/core/components/calendar/calendar.component.ts
+++ src/app/core/components/calendar/calendar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { DateAdapter } from '@angular/material/core';
+import { ICalendarDay } from 'src/app/interfaces/icalendar-day';
 
 @Component({
   selector: 'app-calendar',
@@ -8,9 +9,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CalendarComponent implements OnInit {
 
-  constructor() { }
+  calendarDays: ICalendarDay[] = [];
+  currentDate: Date = new Date();
+
+  constructor(private dateAdapter: DateAdapter<Date>) { }
 
   ngOnInit(): void {
   }
 
+  generateCalendar(date: Date): void {
+    this.calendarDays = [];
+    
+    const startOfMonth = this.dateAdapter.createDate(date.getFullYear(), date.getMonth(), 1);
+    const today = new Date();
+    const startDayOfWeek = this.dateAdapter.getDayOfWeek(startOfMonth);
+    const daysToPrepend = startDayOfWeek;
+
+    let dateIterator = this.dateAdapter.addCalendarDays(startOfMonth, -daysToPrepend);
+
+    for (let i = 0; i < 42; i++){
+      const currentMonth = date.getMonth();
+      const isCurrentMonth = dateIterator.getMonth() === currentMonth;
+
+      this.calendarDays.push({
+        date: dateIterator,
+        dayOfMonth: this.dateAdapter.getDate(dateIterator),
+        isCurrentMonth: isCurrentMonth,
+        isToday: this.dateAdapter.sameDate(dateIterator, today),
+        dayOfWeek: this.dateAdapter.getDayOfWeek(dateIterator)
+      })
+    }
+
+      dateIterator = this.dateAdapter.addCalendarDays(dateIterator, 1);
+    
+  }
 }
